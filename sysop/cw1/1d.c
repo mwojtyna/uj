0d3f878e2530eba2b02573665548b297fc818ba3
Mateusz Wojtyna <w_mateusz@hotmail.com>
2025-03-07 09:46:49 +0100

    feat(sysop): add 1d

--- sysop/cw1/1d.c
+++ sysop/cw1/1d.c
@@ -5,20 +5,28 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+#define N 3
+
 int main(void) {
     printProcessData("Rodzic");
 
-    for (int i = 0; i < 3; i++) {
+    int gen = 0;
+    for (int i = 0; i < N; i++) {
         pid_t pid = fork();
         if (pid == -1) {
             perror("Fork error");
             exit(1);
         } else if (pid == 0) {
             // Potomek
-            sleep(3); // 3 sekundy żebym miał czas na uruchomienie `pstree`
-            printProcessData("Potomek");
+            sleep(gen);
+            gen++;
+
+            char name[64];
+            sprintf(name, "Potomek poziomu %d", gen);
+            printProcessData(name);
         }
     }
 
+    sleep(N + 1);
     return 0;
 }
