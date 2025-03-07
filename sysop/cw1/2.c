f260b98b1d05f289b14354cc2306870899cfe2b4
Mateusz Wojtyna <w_mateusz@hotmail.com>
2025-03-07 13:41:17 +0100

    feat(sysop): add 2

---
+++ sysop/cw1/2.c
@@ -0,0 +1,36 @@
+#include "lib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define N 3
+
+int main(int argc, char** argv) {
+    if (argc == 1) {
+        printf("Wymagana jest ścieżka programu do uruchomienia w potomku!");
+        exit(1);
+    }
+
+    printProcessData("Rodzic");
+
+    for (int i = 0; i < N; i++) {
+        pid_t pid = fork();
+        if (pid == -1) {
+            perror("Fork error");
+            exit(1);
+        } else if (pid == 0) {
+            // Potomek
+            if (execlp(argv[1], NULL) == -1) {
+                perror("execlp error");
+            }
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        wait(NULL);
+    }
+
+    return 0;
+}
--- sysop/cw1/potomek.c
+++ sysop/cw1/potomek.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-#include <sys/types.h>
 #include <unistd.h>
 
-void printProcessData(char* name) {
+int main(int argc, char** argv) {
     uid_t uid = getuid();
     gid_t gid = getgid();
     pid_t pid = getpid();
     pid_t ppid = getppid();
     pid_t pgid = getpgrp();
 
-    printf("%s: UID=%u, GID=%u, PID=%u, PPID=%u, PGID=%u\n", name, uid, gid,
-           pid, ppid, pgid);
+    printf("Potomek: UID=%u, GID=%u, PID=%u, PPID=%u, PGID=%u\n", uid, gid, pid,
+           ppid, pgid);
+
+    return 0;
 }
