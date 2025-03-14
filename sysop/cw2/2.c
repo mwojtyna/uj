54c4077b3da4b022d6b964b82311a91be6b9c3f2
Mateusz Wojtyna <w_mateusz@hotmail.com>
2025-03-14 08:52:04 +0100

    chore(sysop/cw2): fix tabs

--- sysop/cw2/2.c
+++ sysop/cw2/2.c
@@ -29,34 +29,35 @@ int main(int argc, char** argv) {
     printProcessData("Rodzic");
 
     for (int i = 0; i < N; i++) {
-		switch(fork()) {
-			case -1: {
-				perror("Fork error");
-				exit(1);
-			}
-			case 0: {
-				uid_t uid = getuid();
-				gid_t gid = getgid();
-				pid_t pid = getpid();
-				pid_t ppid = getppid();
-				pid_t pgid = getpgrp();
+        switch (fork()) {
+        case -1: {
+            perror("Fork error");
+            exit(1);
+        }
+        case 0: {
+            uid_t uid = getuid();
+            gid_t gid = getgid();
+            pid_t pid = getpid();
+            pid_t ppid = getppid();
+            pid_t pgid = getpgrp();
 
-				char text[128];
-				sprintf(text, "Potomek: UID=%u, GID=%u, PID=%u, PPID=%u, PGID=%u", uid, gid, pid, ppid, pgid);
+            char text[128];
+            sprintf(text, "Potomek: UID=%u, GID=%u, PID=%u, PPID=%u, PGID=%u",
+                    uid, gid, pid, ppid, pgid);
 
-				if (execlp(argv[1], argv[1], text, NULL) == -1) {
-					perror("execlp error");
-				}
-				break;
-			}
-			default: {
-				if (wait(NULL) == -1) {
-					perror("wait() error");
-					exit(1);
-				}
-				break;
-			}
-		}
+            if (execlp(argv[1], argv[1], text, NULL) == -1) {
+                perror("execlp error");
+            }
+            break;
+        }
+        default: {
+            if (wait(NULL) == -1) {
+                perror("wait() error");
+                exit(1);
+            }
+            break;
+        }
+        }
     }
 
     return 0;
