a079751ffa75afd8ec66d610865359611f86163c
Mateusz Wojtyna <w_mateusz@hotmail.com>
2025-03-28 09:46:04 +0100

    feat(sysop/cw3): 3b done

--- sysop/cw3/3b.c
+++ sysop/cw3/3b.c
@@ -1,5 +1,10 @@
+#define _GNU_SOURCE
+
+#include <errno.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
@@ -13,8 +18,24 @@
 // zawarty w statusie zakonczenia, a takze jego nazwe (uzyc funkcji strsignal). Do
 // wyluskania numeru sygnalu ze statusu zakonczenia uzyc makr opisanych w podroz-
 // dziale 2.4.
-int main() {
-    switch (fork()) {
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        printf("Niepoprawna liczba argumentów\n");
+        printf("%s <nr_sygnału> <operacja>\n", argv[0]);
+        printf("<operacja>:\n");
+        printf(" - 0: operacja domyślna\n");
+        printf(" - 1: zignorowanie sygnału\n");
+        printf(" - 2: przechywcenie sygnału\n");
+        exit(1);
+    }
+
+    int sig;
+    if (sscanf(argv[1], "%d", &sig) != 1) {
+        printf("Niepoprawny nr sygnału: %s", argv[1]);
+    }
+
+    int childPid = fork();
+    switch (childPid) {
         case -1:
             perror("fork() error");
             exit(1);
@@ -22,14 +43,36 @@ int main() {
 
         // Potomek
         case 0:
-            execlp("./3a.x", "./3a.x", "10", "2", NULL);
+            if (execlp("./3a.x", "./3a.x", argv[1], argv[2], NULL) == -1) {
+                perror("execlp() error");
+                _exit(1);
+            }
             break;
 
         // Macierzysty
         default:
-            if (wait(NULL) == -1) {
+            sleep(1);
+
+            // Sprawdź czy proces istneje
+            if (kill(childPid, 0) == -1) {
+                perror("kill() error");
+                exit(1);
+            }
+
+            if (kill(childPid, sig) == -1) {
+                perror("kill() error");
+                exit(1);
+            }
+
+            int status;
+            if (wait(&status) == -1) {
                 perror("wait() error");
                 exit(1);
+            }
+
+            if (WIFSIGNALED(status)) {
+                int killedBy = WTERMSIG(status);
+                printf("Proces potomny zabity przez %s", strsignal(killedBy));
             }
             break;
     }
