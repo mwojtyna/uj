6442efb8df7cf27997b3b37a59bf1a4a31b9cf2c
Mateusz Wojtyna <w_mateusz@hotmail.com>
2025-10-23 14:36:29 +0200

    refactor(java/zad01): remove main function, make class private

--- java/Zadanie01/src/Sequence.java
+++ java/Zadanie01/src/Sequence.java
@@ -1,55 +1,9 @@
 import java.util.Arrays;
 
-public class Sequence extends AbstractSequence {
+class Sequence extends AbstractSequence {
     // Encoded
     private int[] data;
     private int deltaSegmentSize;
-
-    public static void main(String[] args) {
-        int[] data1 = {1, 2, 3};
-        int[] data2 = {1, 2, 3};
-        AbstractSequence sequence = new Sequence();
-        sequence.sequence(data2, 3);
-
-        int[] decoded = sequence.decode();
-        int[] encoded = sequence.encode(3);
-        System.out.println(sequence.equals(data2, 3));
-        System.out.println(sequence.equals(data2, 2));
-        System.out.println(Arrays.toString(decoded));
-        System.out.println(Arrays.toString(encoded));
-
-        sequence.sequence(data1, 2);
-        decoded = sequence.decode();
-        encoded = sequence.encode(2);
-        System.out.println(sequence.equals(data1, 2));
-        System.out.println(sequence.equals(data1, 1));
-        System.out.println(Arrays.toString(decoded));
-        System.out.println(Arrays.toString(encoded));
-
-        sequence.sequence(data1, 1);
-        decoded = sequence.decode();
-        encoded = sequence.encode(1);
-        System.out.println(sequence.equals(data1, 1));
-        System.out.println(Arrays.toString(decoded));
-        System.out.println(Arrays.toString(encoded));
-
-        sequence.sequence(data1, 0);
-        decoded = sequence.decode();
-        encoded = sequence.encode(0);
-        System.out.println(sequence.equals(data1, 0));
-        System.out.println(Arrays.toString(decoded));
-        System.out.println(Arrays.toString(encoded));
-
-        sequence.sequence(data1, 12);
-        decoded = sequence.decode();
-        encoded = sequence.encode(12);
-        System.out.println(sequence.equals(data1, 12));
-
-        sequence.sequence(data1, 13);
-        decoded = sequence.decode();
-        encoded = sequence.encode(13);
-        System.out.println(sequence.equals(data1, 13));
-    }
 
     @Override
     public void sequence(int[] data, int deltaSegmentSize) {
