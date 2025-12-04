733fb9a6ba51b1df0c6cbe150de1030b7ead0a77
Mateusz Wojtyna <w_mateusz@hotmail.com>
2025-12-04 12:06:31 +0100

    feat(java/zad07): load scoring

--- java/Zadanie07/src/JavaGradesHelper.java
+++ java/Zadanie07/src/JavaGradesHelper.java
@@ -1,17 +1,24 @@
 import java.io.*;
-import java.nio.CharBuffer;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class JavaGradesHelper implements GradesHelper {
-    private final List<Student> students = new ArrayList<>();
+    private final int SCORING_COLS = 3;
+    private final Map<Student, Integer> studentToId = new HashMap<>();
+    private final Map<String, GradeRange> grades = new HashMap<>(); // grade name -> score range
 
     public static void main(String[] args) {
         {
             JavaGradesHelper grading = new JavaGradesHelper();
-            grading.loadStudents("./src/studenci.txt");
+
+            grading.loadStudents("./src/students.txt");
+            System.out.println(grading.studentToId);
+
+            try {
+                grading.loadScoring("./src/scoring.txt");
+            } catch (RangeConflictException | MarkConflictException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(grading.grades);
         }
     }
 
@@ -19,28 +26,36 @@ public class JavaGradesHelper implements GradesHelper {
     public void loadStudents(String file) {
         try {
             FileReader reader = new FileReader(file);
-            while (true) {
-                String line = readUntil(reader, ';');
-                if (line.isEmpty()) {
-                    break;
-                }
-
-                String[] parts = line.split(",");
+            String line;
+            while (!(line = readUntil(reader, ';')).isEmpty()) {
+                String[] parts = line.split("[,;]");
                 if (parts.length != 3) {
                     continue;
                 }
 
-                this.students.add(new Student(Integer.parseInt(parts[0]), parts[1], parts[2]));
+                int id = Integer.parseInt(parts[0]);
+                var student = new Student(parts[1], parts[2]);
+                this.studentToId.put(student, id);
             }
         } catch (IOException ignored) {
         }
-
-        System.out.println(students);
     }
 
     @Override
     public void loadScoring(String file) throws RangeConflictException, MarkConflictException {
-
+        try {
+            FileReader reader = new FileReader(file);
+            String line;
+            while (!(line = readUntil(reader, ';', SCORING_COLS)).isEmpty()) {
+                String[] parts = line.split(";");
+                if (parts.length != 3) {
+                    continue;
+                }
+                this.grades.put(parts[0], new GradeRange(Double.parseDouble(parts[1]), Double.parseDouble(parts[2])));
+                // TODO: handle exceptions
+            }
+        } catch (IOException ignored) {
+        }
     }
 
     @Override
@@ -49,22 +64,33 @@ public class JavaGradesHelper implements GradesHelper {
     }
 
     private String readUntil(Reader reader, char until) throws IOException {
+        return readUntil(reader, until, 1);
+    }
+
+    private String readUntil(Reader reader, char until, int nOccurrences) throws IOException {
         int c;
+        int occurrences = 0;
         StringBuilder builder = new StringBuilder();
+
         while ((c = reader.read()) != -1) {
             if (c == '\n') {
                 continue;
             }
 
-            if (c == until) {
-                return builder.toString();
-            }
             builder.append((char) c);
+            if (c == until) {
+                if (++occurrences == nOccurrences) {
+                    return builder.toString();
+                }
+            }
         }
 
         return builder.toString();
     }
 }
 
-record Student(int id, String name, String lastName) {
+record Student(String name, String lastName) {
+}
+
+record GradeRange(double min, double max) {
 }
