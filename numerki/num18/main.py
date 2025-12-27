b50099e9e85d4f846c285947a0c1ab49144152a6
Mateusz Wojtyna <w_mateusz@hotmail.com>
2025-12-27 20:04:29 +0100

    fix(numerki/zad18): just input the correct polynomial coefficients

--- numerki/num18/main.py
+++ numerki/num18/main.py
@@ -6,34 +6,32 @@ array = NDArray[num]
 vector = NDArray[num]
 
 
-def P(x: float, a: vector):
-    N = len(a)
+def f(x: float):
+    coeffs = [1, -1, 0, 0, 3, -2, 0, 1]
+    p = coeffs[-1]
 
-    y = 0
-    tmp = 1
-    for i in range(N):
-        y += a[i] * tmp
-        tmp *= x
+    for k in range(len(coeffs) - 2, -1, -1):
+        p = p * x + coeffs[k]
 
-    return y
+    return p
 
 
-def initial_interval(x_0: num, coeffs, delta=0.1, limit=100):
+def initial_interval(x_0: num, delta=0.1, limit=100):
     a = x_0
-    fa = P(a, coeffs)
+    fa = f(a)
     b = a + delta
-    fb = P(b, coeffs)
+    fb = f(b)
 
     if fb > fa:
         # zmiana kierunku
         delta *= -1
         b = a + delta
-        fb = P(b, coeffs)
+        fb = f(b)
         assert fa > fb
 
     for _ in range(limit):
         c = b + delta
-        fc = P(c, coeffs)
+        fc = f(c)
 
         if fc > fb:
             return a, b, c
@@ -47,14 +45,14 @@ def initial_interval(x_0: num, coeffs, delta=0.1, limit=100):
     raise RuntimeError("Nie znaleziono przedziału")
 
 
-def brent(a: num, b: num, c: num, coeffs: array, limit=100, eps=1e-6):
+def brent(a: num, b: num, c: num, limit=100, eps=1e-6):
     prev = abs(c - a)
     prev2 = prev
 
     for i in range(limit):
-        fa = P(a, coeffs)
-        fb = P(b, coeffs)
-        fc = P(c, coeffs)
+        fa = f(a)
+        fb = f(b)
+        fc = f(c)
         old_b = b
         d = 0
 
@@ -73,7 +71,7 @@ def brent(a: num, b: num, c: num, coeffs: array, limit=100, eps=1e-6):
         if abs(c - a) < eps * (abs(old_b) + abs(d)):
             return d, i + 1
 
-        fd = P(d, coeffs)
+        fd = f(d)
         if fd < fb:
             if d < b:
                 c = b
@@ -93,63 +91,9 @@ def brent(a: num, b: num, c: num, coeffs: array, limit=100, eps=1e-6):
     raise RuntimeError("Nie znaleziono punktu (brent)")
 
 
-def lagrange_basis(j: int, x: float, nodes: vector) -> float:
-    N = len(nodes)
-    nominator = 1.0
-    denominator = 1.0
-
-    for i in range(0, j):
-        nominator *= x - nodes[i]
-        denominator *= nodes[j] - nodes[i]
-    for i in range(j + 1, N):
-        nominator *= x - nodes[i]
-        denominator *= nodes[j] - nodes[i]
-
-    return nominator / denominator
-
-
-def lagrange_polynomial(nodes: vector, values: vector) -> vector:
-    """
-    Zwraca współczynniki wielomianu interpolacyjnego Lagrange'a
-    """
-    N = len(nodes)
-    a = np.zeros(N, dtype=num)
-    l = np.zeros(N, dtype=num)
-    values = values.copy()
-
-    # k = 0
-    for j in range(N):
-        l[j] = lagrange_basis(j, 0, nodes)
-        a[0] += l[j] * values[j]
-
-    # k > 0
-    for k in range(1, N):
-        for j in range(N):
-            values[j] = (values[j] - a[k - 1]) / nodes[j]
-            a[k] += l[j] * values[j]
-
-    return a
-
-
 def main():
-    nodes = np.array([-1, -0.75, -0.5, -0.25, 0.25, 0.5, 0.75, 1], dtype=num)
-    values = np.array(
-        [
-            6,
-            3.04034423828125,
-            1.7421875,
-            1.26361083984375,
-            0.75982666015625,
-            0.6328125,
-            0.85809326171875,
-            2,
-        ],
-        dtype=num,
-    )
-    coeffs: vector = lagrange_polynomial(nodes, values)
-
-    a, b, c = initial_interval(0, coeffs)
-    value, steps = brent(a, b, c, coeffs)
+    a, b, c = initial_interval(0)
+    value, steps = brent(a, b, c)
     print(f"{value} po {steps} krokach")
 
 
