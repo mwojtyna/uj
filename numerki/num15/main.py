41499561705473b45b31850eb1e6d93a5caae4c9
Mateusz Wojtyna <w_mateusz@hotmail.com>
2026-01-16 17:33:41 +0100

    fix(numerki/num15): some issues with roots

--- numerki/num15/main.py
+++ numerki/num15/main.py
@@ -6,14 +6,16 @@ array = NDArray[np.complex128]
 num = np.complex128
 
 
-# Zwraca: f(z), f'(z), f''(z)
 def horner(z: num, coeffs: array):
+    """
+    Zwraca (f(z), f'(z), f''(z))
+    """
     N = len(coeffs) - 1
-    p = coeffs[-1]  # f(z)
+    p = coeffs[0]  # f(z)
     p_prime = 0  # f'(z)
     p_prime2 = 0  # f''(z)
 
-    for k in range(N - 1, -1, -1):
+    for k in range(1, N + 1):
         p_prime2 = p_prime + z * p_prime2
         p_prime = p + z * p_prime
         p = p * z + coeffs[k]
@@ -43,31 +45,45 @@ def laguerre(z: num, coeffs: array, eps=1e-8, limit=1000):
 
 
 def forward_substitution(coeffs: vector, z_0: num) -> vector:
+    """
+    Dzieli wielomian P(z) przez (z - z_0).
+    Zwraca wspolczynniki nowego wielomianu o stopniu n-1.
+    """
     n = len(coeffs) - 1
+    new_coeffs = np.zeros(n, dtype=num)
 
-    x = np.zeros(n, dtype=num)
-    x[0] = coeffs[-1]
+    new_coeffs[0] = coeffs[0]
     for i in range(1, n):
-        x[i] = coeffs[n - i] + z_0 * x[i - 1]
+        new_coeffs[i] = coeffs[i] + z_0 * new_coeffs[i - 1]
 
-    return x
+    return new_coeffs
 
 
 def calc_roots(coeffs: array):
     roots = []
     steps_list = []
-    while len(coeffs) > 3:
-        root, steps = laguerre(0, coeffs)
-        roots.append(root)
-        steps_list.append(steps)
-        coeffs = forward_substitution(coeffs, root)
+    original_coeffs = coeffs.copy()
 
-    roots.extend(np.roots(coeffs))  # Ostatnie 2 liczymy z delty
+    while len(coeffs) > 3:
+        # 1. Policzenie Laguerrem
+        root, steps = laguerre(0, coeffs)
+        steps_list.append(steps)
+
+        # 2. Wygładzenie
+        wygladzony_root, s = laguerre(root, original_coeffs)
+        roots.append(wygladzony_root)
+
+        # 3. Deflacja
+        coeffs = forward_substitution(coeffs, wygladzony_root)
+
+    # Ostatnie 2 pierwiastki liczymy analitycznie
+    roots.extend(np.roots(coeffs))
+
     return roots, steps_list
 
 
 def main():
-    coeffs = np.array([1, -1j, -1, 1j, 1], dtype=num)  # a_0, ... ,a_n
+    coeffs = np.array([1, 1j, -1, -1j, 1], dtype=num)  # a_n, ... ,a_0
     roots, steps = calc_roots(coeffs)
     print(roots, steps)
 
